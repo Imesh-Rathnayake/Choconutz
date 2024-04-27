e8f08f9e4c70151d6299501d90e2637eec937839
PawaniMadhushani <122030540+PawaniMadhushani@users.noreply.github.com>
2024-04-27 12:55:15 +0530

    Update Customer Login Servlet

--- project/src/java/CustomerServlet/customerLoginservlet.java
+++ project/src/java/CustomerServlet/customerLoginservlet.java
@@ -5,8 +5,10 @@
  */
 package CustomerServlet;
 
+import CustomerController.customerController;
 import java.io.IOException;
 import java.io.PrintWriter;
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -15,20 +17,12 @@ import javax.servlet.http.HttpServletResponse;
 
 /**
  *
- * @author User
+ * @author asus
  */
-@WebServlet(name = "customerLoginservlet", urlPatterns = {"/customerLoginservlet"})
-public class customerLoginservlet extends HttpServlet {
+@WebServlet(name = "customerRegisterServlet", urlPatterns = {"/customerRegisterServlet"})
+public class customerRegisterServlet extends HttpServlet {
 
-    /**
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
-     * methods.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
+ 
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
@@ -37,49 +31,59 @@ public class customerLoginservlet extends HttpServlet {
             out.println("<!DOCTYPE html>");
             out.println("<html>");
             out.println("<head>");
-            out.println("<title>Servlet customerLoginservlet</title>");            
+            out.println("<title>Servlet customerRegisterServlet</title>");            
             out.println("</head>");
             out.println("<body>");
-            out.println("<h1>Servlet customerLoginservlet at " + request.getContextPath() + "</h1>");
+            out.println("<h1>Servlet customerRegisterServlet at " + request.getContextPath() + "</h1>");
             out.println("</body>");
             out.println("</html>");
         }
     }
 
-    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-    /**
-     * Handles the HTTP <code>GET</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
+ 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         processRequest(request, response);
     }
 
-    /**
-     * Handles the HTTP <code>POST</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
+  
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        processRequest(request, response);
+//        processRequest(request, response);
+          String username = request.getParameter("uname");
+          String email = request.getParameter("email");
+          String phone = request.getParameter("phone");
+          String password = request.getParameter("pwd");
+          
+          
+          
+          customerController c1 = new customerController();
+          String massage = c1.customerRegister(username,email,phone,password);
+          
+          if(massage.equals("true")){
+           request.setAttribute("message", "Register successfully");
+             RequestDispatcher dispatcher = request.getRequestDispatcher("register.jsp");
+             dispatcher.forward(request, response);
+
+
+          }else if(massage.equals("false")); {
+                
+                request.setAttribute("message", "user already register");
+                RequestDispatcher dispatcher=request.getRequestDispatcher("register.jsp");
+                dispatcher.forward(request,response);
+            }
+          
+          else {
+                  request.setAttribute("message", message);
+                  RequestDispatcher dispatcher=request.getRequestDispatcher("register.jsp");
+                  dispatcher.forward(request, response);
+                  }
+          
     }
 
-    /**
-     * Returns a short description of the servlet.
-     *
-     * @return a String containing servlet description
-     */
+ 
     @Override
     public String getServletInfo() {
         return "Short description";
